2e9d73ebacddc755eaa1134225ec0f49a058cfb0
mroshaw <oli@mroshaw.co.uk>
2026-08-18 21:01:49 +0100

    Added Observatory Aquarium prefab, with configurator and custom model

---
+++ Assets/Mods/MoreAquariums_BZ/Scripts/MoreAquariums/ObservatoryAquariumPrefab.cs
@@ -0,0 +1,43 @@
+using Nautilus.Assets;
+using Nautilus.Crafting;
+using UnityEngine;
+
+namespace DaftAppleGames.MoreAquariums
+{
+    /// <summary>
+    /// Describes the prototype Observatory Aquarium base piece.
+    /// </summary>
+    public class ObservatoryAquariumPrefab : BaseAquariumPrefab
+    {
+        private const string ClassId = "ObservatoryAquarium";
+        private const string DisplayName = "Observatory Aquarium";
+        private const string Description =
+            "A prototype aquarium base piece built from an Observatory.";
+        private const string ConfigurationPrefabAssetName =
+            "ObservatoryAquariumPrefab.prefab";
+
+        private static readonly RecipeData Recipe = new RecipeData(
+            new Ingredient(TechType.Titanium, 2),
+            new Ingredient(TechType.EnameledGlass, 2));
+
+        public static PrefabInfo PrefabInfo { get; private set; }
+
+        /// <summary>
+        /// Registers the Observatory Aquarium prototype.
+        /// </summary>
+        public static void Register()
+        {
+            PrefabInfo = RegisterInternal(
+                ClassId, DisplayName, Description, Recipe);
+        }
+
+        /// <summary>
+        /// Adds aquarium functionality to a completed Observatory Aquarium module.
+        /// </summary>
+        internal static void ConfigureCompletedBasePiece(GameObject basePieceGameObject)
+        {
+            BaseAquariumConfigurator.ConfigureFromAssetBundle(
+                basePieceGameObject, ConfigurationPrefabAssetName);
+        }
+    }
+}
